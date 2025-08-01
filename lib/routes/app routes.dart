0590231aef780e90c31b429146f9fd4e1fe0e26b
Akshatagandhewar <akshatagandhewar@gmail.com>
2025-08-01 11:33:36 +0530

    battery

--- lib/routes/app routes.dart
+++ lib/routes/app routes.dart
@@ -1,15 +1,15 @@
 class AppRoutes {
   static const String firstScreen = '/';
   static const String login_Screen = '/Loginpage_';
-  static const String forgot= '/forgot_password_';
-  static const String sign_up= 'Sign up_';
-  static const String Homepage= '/homepage_';
-  static const String profile= '/_';
-  static const String recipe= '/_';
-  static const String saved= '/_';
-  static const String scan= '/_';
-  static const String speak= '/';
-  static const String Suggession=
-  static const String tabs=
-  static const String text=
+  static const String forgot = '/forgot_password_';
+  static const String sign_up = 'Sign up_';
+  static const String Homepage = '/homepage_';
+  static const String profile = '/profile_';
+  static const String recipe = '/recipe_';
+  static const String saved = '/saved_';
+  static const String scan = '/scan_';
+  static const String speak = '/speak';
+  static const String Suggession = 'suggession';
+  static const String tabs = '/tabs';
+  static const String text = '/text';
 }
