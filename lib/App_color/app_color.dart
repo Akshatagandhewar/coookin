2882856f67b6fa9447093b8b9467d14fd76629c5
Akshatagandhewar <akshatagandhewar@gmail.com>
2025-08-01 10:29:25 +0530

    Message

---
+++ lib/App_color/app_color.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+
+class AppColor {
+  static const Color primarycolor = Color.fromARGB(255, 2, 1, 36);
+  static const Color secondarycolor = Color.fromARGB(255, 66, 65, 78);
+  static const Color textColor = Colors.black;
+
+  
+}
---
+++ lib/routes/app routes.dart
@@ -0,0 +1,15 @@
+class AppRoutes {
+  static const String firstScreen = '/';
+  static const String  = '/createAccount_';
+  static const String  = '/ForgotPassword_';
+  static const String  = '/Library_';
+  static const String  = '/login_screen_';
+  static const String  = '/notificationscreen_';
+  static const String  = '/Shorts_';
+  static const String  = '/Subscription_';
+  static const String  = '/tab_';
+  static const String  = '/YouTube_play';
+  static const String  =
+  static const String  =
+  static const String
+}
